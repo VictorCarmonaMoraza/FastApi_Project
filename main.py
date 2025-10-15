a9b3e142cd595e975232d24fdb183f97b8e32ca1
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-15 20:38:58 +0200

    Agregamos pantalla de inicio

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ app.include_router(routerUser)
 
 Base.metadata.create_all(bind=engine)
 
+
+@app.get("/")
+def read_root():
+    return HTMLResponse('<h2>Welcome to my API</h2>')
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
